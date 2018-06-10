a5a0fdaab79c666c083c8eba1e9b3bf6353eb499
yowari <yowari@users.noreply.github.com>
2018-06-10 13:59:10 +0200

    fix monaco assets baseUrl

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,15 @@
-import { AuthGuard } from './services/auth-guard.service';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, PlatformLocation, Location } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MonacoEditorModule } from 'ngx-monaco-editor';
+import { MonacoEditorModule, NgxMonacoEditorConfig, NGX_MONACO_EDITOR_CONFIG } from 'ngx-monaco-editor';
 import { ShowdownModule } from 'ngx-showdown';
 import { UserService } from './services/user.service';
 import { AuthService } from './services/auth.service';
+import { AuthGuard } from './services/auth-guard.service';
 import { ChallengeService } from './services/challenge.service';
 
 @NgModule({
@@ -20,7 +20,7 @@ import { ChallengeService } from './services/challenge.service';
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     NgbModule.forRoot(),
-    MonacoEditorModule.forRoot(),
+    MonacoEditorModule,
     ShowdownModule
   ],
   declarations: [],
@@ -31,14 +31,27 @@ import { ChallengeService } from './services/challenge.service';
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     NgbModule.forRoot().ngModule,
-    MonacoEditorModule.forRoot().ngModule,
+    MonacoEditorModule,
     ShowdownModule
   ],
   providers: [
     UserService,
     AuthService,
     AuthGuard,
-    ChallengeService
+    ChallengeService,
+    {
+      provide: NGX_MONACO_EDITOR_CONFIG,
+      useFactory: getMonacoConfig,
+      deps: [PlatformLocation]
+    }
   ]
 })
 export class SharedModule { }
+
+export function getMonacoConfig(platformLocation: PlatformLocation): NgxMonacoEditorConfig {
+  const baseHref = platformLocation.getBaseHrefFromDOM();
+
+  return {
+    baseUrl: Location.joinWithSlash(baseHref, '/assets')
+  }
+}
